7be04c7fb55e10b74098bfd8003f8d61a1eb644e
Abdelmjid-ASOUAB <abde.Asouab@gmail.com>
2022-11-23 15:17:05 +0100

    create HttpResponse

---
+++ lib/src/models/http_response.dart
@@ -0,0 +1,16 @@
+class HttpResponse {
+  String body;
+  int statusCode;
+  String message;
+
+  HttpResponse({required this.statusCode, required this.body, this.message = ''});
+
+  bool get isSuccess {
+    return statusCode >= 200 && statusCode < 300;
+  }
+
+  @override
+  String toString() {
+    return 'HttpResponse{statusCode: $statusCode, success: $isSuccess, body: $body, message: $message}';
+  }
+}
