e3245a1b78e407054f14a7241cfb342d35c41108
Abdelmjid-ASOUAB <abde.Asouab@gmail.com>
2022-11-26 11:49:45 +0100

    add toString to Exception

--- lib/src/exceptions/invalid_decoded_json_exception.dart
+++ lib/src/exceptions/invalid_decoded_json_exception.dart
@@ -2,4 +2,9 @@ class InvalidDecodedJSONException implements Exception {
   final String message;
 
   InvalidDecodedJSONException(this.message);
+
+  @override
+  String toString() {
+    return 'InvalidDecodedJSONException: $message';
+  }
 }
--- lib/src/exceptions/invalid_response_exception.dart
+++ lib/src/exceptions/invalid_response_exception.dart
@@ -2,4 +2,9 @@ class InvalidResponseException implements Exception {
   final String message;
 
   InvalidResponseException(this.message);
+
+  @override
+  String toString() {
+    return 'InvalidResponseException: $message';
+  }
 }
