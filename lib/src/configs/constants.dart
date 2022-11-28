dd4d5baf0576db9dd65e5d7b179d4571444641ca
Abdelmjid-ASOUAB <abde.Asouab@gmail.com>
2022-11-28 16:32:01 +0100

    add urls

--- lib/src/configs/constants.dart
+++ lib/src/configs/constants.dart
@@ -1,5 +1,5 @@
 class Constants {
-  static String BASE_URL = "https://run.mocky.io/v3/";
-  static String CONFIG_URL = "ec7547e6-43f0-448b-8ece-ef971c03b5e0";
-  static String PAY_WITH_CARD_URL = "5d7d05e8-4110-428a-9cec-35f58adfca7c";
+  static String BASE_URL = "https://youcanpay.com/api/";
+  static String CONFIG_URL = "et-account-configs/";
+  static String PAY_WITH_CARD_URL ="pay/";
 }
