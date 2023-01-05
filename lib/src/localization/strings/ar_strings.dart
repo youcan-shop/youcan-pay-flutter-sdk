82ff86d562d9c4612c43bba0b394385fd967eb57
Abdelmjid-ASOUAB <abde.asouab@gmail.com>
2023-01-05 10:31:44 +0100

    create ycp_strings

---
+++ lib/src/localization/strings/ar_strings.dart
@@ -0,0 +1,11 @@
+const Map<String, String> arabic = {
+  "unexpected_error_occurred": "حدث خطأ أثناء الدفع.",
+  "payment_canceled": "تم إلغاء الدفع",
+  "payment_with_card_invalid": "الدفع ببطاقة الائتمان غير مفعل لحسابك.",
+  "payment_with_cashplus_invalid": "الدفع باستخدام CashPlus غير مفعل لحسابك.",
+  "card_number_is_empty": "رقم البطاقة فارغ",
+  "card_number_is_not_valid": "رقم البطاقة غير صالح",
+  "card_expiry_date_is_not_valid": "تاريخ انتهاء صلاحية البطاقة غير صالح",
+  "card_cvv_is_not_valid": "CVV البطاقة غير صالح",
+  "payment_failed": "فشل الدفع",
+};
