2160a576f02755d94333174e218fed7c68b73412
Abdelmjid-ASOUAB <abde.asouab@gmail.com>
2023-01-06 14:23:02 +0100

    add cashplus url

--- lib/src/configs/constants.dart
+++ lib/src/configs/constants.dart
@@ -2,4 +2,5 @@ class Constants {
   static String BASE_URL = "https://youcanpay.com/api/";
   static String CONFIG_URL = "get-account-configs/";
   static String PAY_WITH_CARD_URL ="pay/";
+  static String PAY_WITH_CASHPLUS_URL ="cashplus/init/";
 }
