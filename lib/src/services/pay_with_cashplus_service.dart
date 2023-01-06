b806476b6555cac69333d68fd0f0221d1316c648
Abdelmjid-ASOUAB <abde.asouab@gmail.com>
2023-01-06 14:56:29 +0100

    cleaning...

--- lib/src/services/pay_with_cashplus_service.dart
+++ lib/src/services/pay_with_cashplus_service.dart
@@ -1,20 +1,18 @@
-import 'package:flutter/material.dart';
-import 'package:youcanpay_sdk/src/configs/constants.dart';
-import 'package:youcanpay_sdk/src/models/ycp_response_cashplus.dart';
-import 'package:youcanpay_sdk/src/models/ycp_response_sale.dart';
-import 'package:youcanpay_sdk/src/models/ycpay_response.dart';
-import 'package:youcanpay_sdk/src/services/based_service.dart';
-
+import '../configs/constants.dart';
+import '../models/ycp_response_cashplus.dart';
+import '../models/ycp_response_sale.dart';
+import '../models/ycpay_response.dart';
+import '../services/based_service.dart';
 import '../factories/ycp_response_factory.dart';
 import '../models/http_response.dart';
 
 class PayWithCashPlusService extends BasedService {
-  void payWithCashPlus({
-    required String token,
-    required String pubKey,
-    required Function(String? transactionId, String? token) onSuccessfulPayment,
-    required Function(String? message) onFailedPayment
-  }) async {
+  void payWithCashPlus(
+      {required String token,
+      required String pubKey,
+      required Function(String? transactionId, String? token)
+          onSuccessfulPayment,
+      required Function(String? message) onFailedPayment}) async {
     Map<String, String> params = {};
 
     params['token_id'] = token;
@@ -22,7 +20,8 @@ class PayWithCashPlusService extends BasedService {
     params['is_mobile'] = "1";
 
     try {
-      HttpResponse response = await httpAdapter.post(url: Constants.PAY_WITH_CASHPLUS_URL, body: params);
+      HttpResponse response = await httpAdapter.post(
+          url: Constants.PAY_WITH_CASHPLUS_URL, body: params);
       YCPayResponse ycPayResponse = YCPResponseFactory.fromJSON(response);
 
       if (ycPayResponse is YCPResponseCashPlus) {
