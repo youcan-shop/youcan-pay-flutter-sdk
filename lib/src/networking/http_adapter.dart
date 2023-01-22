6b50b48f3c80fb79e1a73771f159a31408697f24
Gwhyyy <ffikhi.aanas@gmail.com>
2023-01-23 00:48:58 +0100

    [ Add ] added comments for interface

--- lib/src/networking/http_adapter.dart
+++ lib/src/networking/http_adapter.dart
@@ -1,11 +1,13 @@
 import '../models/http_response.dart';
 
 abstract class HttpAdapter {
+  /// Sends a POST request to the given [url] with the given [body].
   Future<HttpResponse> post({
     required String url,
     Map<String, String> body = const {},
   });
 
+  /// Sends a GET request to the given [url] with the given [params].
   Future<HttpResponse> get({
     required String url,
     Map<String, String> params = const {},
