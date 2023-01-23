13ac49e0b6614e0b9c00173dd04981c2bb57d887
Gwhyyy <ffikhi.aanas@gmail.com>
2023-01-23 01:33:57 +0100

    [ Add, Edit ] added comments to the CardInformationValidator members, removed the implmentation of the isCardExpiryValid since it was wrong, and complex for a developer to understand, exposed the use if built-in DateTime methods, added an assertion to make sure that the given year, month strings can be parsed to a real integer, otherwise, a wrong input will crash the app using the library on the error, commented the extension methods

--- lib/src/validators/card_information_validator.dart
+++ lib/src/validators/card_information_validator.dart
@@ -4,58 +4,52 @@ import '../models/card_information.dart';
 
 class CardInformationValidator {
   static const String _cardNumberRegex = r'^[0-9]{16}$';
-  static const String _cardExpiryRegex = r'^[0-9]{2}$';
   static const String _cardCvvRegex = r'^[0-9]{3}$';
 
+  /// returns true if the [cardNumber] is valid.
   static bool isCardNumberValid(String cardNumber) {
     return RegExp(_cardNumberRegex).hasMatch(cardNumber);
   }
 
+  /// returns true if the [cardCvv] is valid.
   static bool isCardCvvValid(String cardCvv) {
     return RegExp(_cardCvvRegex).hasMatch(cardCvv);
   }
 
+  /// returns true if the [year] and [month] is valid.
   static bool isCardExpiryValid({required String year, required String month}) {
+    final int? yearInt = int.tryParse(year), monthInt = int.tryParse(month);
+    assert(
+      yearInt != null && monthInt != null,
+      "year and month must be able to be parsed to int,",
+    );
+
+    DateTime expiresDate = DateTime(2000 + yearInt!, monthInt!);
     DateTime now = DateTime.now();
 
-    int currentYear = now.year;
-    int currentMonth = now.month;
-
-    if (RegExp(_cardExpiryRegex).hasMatch(year) &&
-        RegExp(_cardExpiryRegex).hasMatch(month)) {
-      DateTime expiryDate = DateTime.parse("20$year-$month-01");
-
-      int expiryYear = expiryDate.year;
-      int expiryMonth = expiryDate.year;
-
-      if (expiryYear > currentYear) {
-        return true;
-      } else if (expiryYear == currentYear) {
-        if (expiryMonth >= currentMonth) {
-          return true;
-        }
-      }
-    }
-
-    return false;
+    return expiresDate.isAfter(now);
   }
 
+  /// Checks if the [cardInformation] is valid.
   static void isValid(CardInformation cardInformation) {
     if (cardInformation.cardHolderName.isEmpty) {
       throw InvalidCardInformationException(
-          YCPayStrings.get("card_number_is_empty"));
+        YCPayStrings.get("card_number_is_empty"),
+      );
     }
 
     if (!isCardNumberValid(cardInformation.cardNumber)) {
       throw InvalidCardInformationException(
-          YCPayStrings.get("card_number_is_not_valid"));
+        YCPayStrings.get("card_number_is_not_valid"),
+      );
     }
 
     if (!isCardExpiryValid(
         year: cardInformation.expireDateYear,
         month: cardInformation.expireDateMonth)) {
       throw InvalidCardInformationException(
-          YCPayStrings.get("card_expiry_date_is_not_valid"));
+        YCPayStrings.get("card_expiry_date_is_not_valid"),
+      );
     }
 
     if (!isCardCvvValid(cardInformation.cvv)) {
@@ -79,22 +73,27 @@ extension ResponseMapValidationExtension on Map<String, dynamic> {
     return containsKey("message");
   }
 
+  /// Checks if the [Map<String, dynamic>] contains a redirect_url or return_url key.
   bool requires3dAuthentication() {
     return containsKey("redirect_url") && containsKey("return_url");
   }
 
+  /// Checks if the [Map<String, dynamic>] contains a token key.
   bool tokenized() {
     return containsKey("token");
   }
 
+  /// Inserts the token to the [Map<String, dynamic>].
   void token(String token) {
     this["token"] = token;
   }
 
+  /// Inserts the pubKey to the [Map<String, dynamic>].
   void pubKey(String pubKey) {
     this["pub_key"] = pubKey;
   }
 
+  /// Inserts the isMobile to the [Map<String, dynamic>].
   void isMobile(String isMobile) {
     this["is_mobile"] = isMobile;
   }
